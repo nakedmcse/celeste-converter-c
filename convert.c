5319929a8d4daaddbf60d074492e026535d05759
Walker Aldridge <daldridge@lairdscomputer.com>
2025-04-29 00:13:55 -0500

    Working data2png

--- convert.c
+++ convert.c
@@ -100,6 +100,48 @@ void getFiles(char *path, StringArray *files) {
 #endif
 }
 
+unsigned char *readFile(char *path) {
+    FILE *file = fopen(path, "rb");
+    if (file == NULL) {
+        printf("Failed to open file: %s\n", path);
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    size_t size = ftell(file);
+    rewind(file);
+    unsigned char *data = malloc(size);
+    if (!data) {
+        printf("Failed to allocate file memory\n");
+        return NULL;
+    }
+
+    size_t bytes_read = fread(data, 1, size, file);
+    if (bytes_read != size) {
+        printf("Failed to read file: %s\n", path);
+        free(data);
+        return NULL;
+    }
+    fclose(file);
+    return data;
+}
+
+char* getFilename(char* path) {
+    char *slash1 = strrchr(path, '/');
+    char *slash2 = strrchr(path, '\\');
+    char *slash = slash1 > slash2 ? slash1 : slash2;
+    return slash ? slash + 1 : path; // If found, return after slash; otherwise, whole path is filename
+}
+
+void setExtension(char *path, char *ext) {
+    char *last_dot = strrchr(path, '.');
+
+    if (last_dot) *last_dot = '\0'; // terminate the string at the dot
+
+    strcat(path, ".");
+    strcat(path, ext);
+}
+
 void convert(char *input, char *output, char *direction) {
     StringArray workfiles;
     workfiles.count = 0;
@@ -123,9 +165,96 @@ void convert(char *input, char *output, char *direction) {
 void data2png(char *input, char *output, bool targetIsFolder) {
     // Implement data to png
     printf("Converting %s to %s%s\n", input, output, targetIsFolder ? " (folder)" : "");
+
+    // Read data file into memory
+    unsigned char *inputFile = readFile(input);
+    if (inputFile == NULL) return;
+    uint32_t width;
+    memcpy(&width, inputFile, sizeof(uint32_t));
+    uint32_t height;
+    memcpy(&height, inputFile + 4, sizeof(uint32_t));
+    bool hasAlpha = (inputFile[8] != 0);
+    printf("Width: %u, Height: %u, Alpha: %d\n", width, height, hasAlpha);
+
+    // Allocate memory for png
+    unsigned char *outputPng = malloc(width * height * 4);
+    if (!outputPng) {
+        printf("Failed to allocate output png\n");
+        return;
+    }
+
+    // Process data file
+    int index = 0;
+    int offset = 9;
+    int pngPos = 0;
+    while (index < width * height) {
+        // Read the RLE count
+        uint8_t count = inputFile[offset++];
+        if (count == 0) {
+            printf("RLE count corrupt\n");
+            return;
+        }
+        // Read RGB(A) channel values
+        uint8_t a, r, g, b;
+        if (hasAlpha) {
+            a = inputFile[offset++];
+            if (a == 0) {
+                r = 0;
+                b = 0;
+                g = 0;
+            }
+            else {
+                b = inputFile[offset++];
+                g = inputFile[offset++];
+                r = inputFile[offset++];
+            }
+        }
+        else {
+            b = inputFile[offset++];
+            g = inputFile[offset++];
+            r = inputFile[offset++];
+            a = 0;
+        }
+
+        // Output RLE span of same color pixels
+        for (int i = 0; i < count; i++) {
+            outputPng[pngPos++] = r;
+            outputPng[pngPos++] = g;
+            outputPng[pngPos++] = b;
+            if (hasAlpha) outputPng[pngPos++] = a;
+        }
+
+        // Increment index by RLE count
+        index += count;
+    }
+
+    // Write png to disk
+    char *inputFileName = getFilename(input);
+    setExtension(inputFileName, "png");
+    char *outputFileName = malloc(strlen(output)+strlen(inputFileName)+3);
+    strncpy(outputFileName, output, strlen(output));
+    if (targetIsFolder) {
+        #ifdef _WIN32
+        outputFileName = strcat(outputFileName, "\\");
+        #else
+        outputFileName = strcat(outputFileName, "/");
+        #endif
+        outputFileName = strcat(outputFileName, inputFileName);
+    }
+    lodepng_encode_file(outputFileName, outputPng, width, height, hasAlpha ? LCT_RGBA : LCT_RGB, 8);
+
+    // Free png and data
+    free(inputFile);
+    free(outputPng);
 }
 
 void png2data(char *input, char *output, bool targetIsFolder) {
     // Implement png to data
     printf("Converting %s to %s%s\n", input, output, targetIsFolder ? " (folder)" : "");
+
+    // Read png file into memory
+    // Allocate memory for data
+    // Process png file
+    // Write data to disk
+    // Free png and data
 }
